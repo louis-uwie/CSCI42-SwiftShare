35030a15278248f88bb2f077f86aa16689ce29fb
franch1se <ronald.francis.bautista@student.ateneo.edu>
2025-03-14 16:42:10 +0800

    Added basic gui with working buttons

--- Main_DesktopSwiftShare/swiftshare/src/main/java/com/csci42_2/App.java
+++ Main_DesktopSwiftShare/swiftshare/src/main/java/com/csci42_2/App.java
@@ -1,13 +1,41 @@
-package com.CSCI42_2;
+package com.csci42_2;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
- * Hello world!
- *
+ * JavaFX App
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("main").load());
+
+        stage.setScene(scene);
+        stage.setMaxWidth(1024);
+        stage.setMaxHeight(768);
+        stage.setResizable(false);
+        stage.show();
     }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml).load());
+    }
+
+    private static FXMLLoader loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
