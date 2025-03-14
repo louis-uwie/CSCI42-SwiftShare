789cdae1318afc677ba928d5ef4a6b912e5b74f1
uwie <157711065+louis-uwie@users.noreply.github.com>
2025-03-14 14:40:06 +0800

    Update MainActivity.java
    Commit includes: Properly initialized buttons. Functional FileLocator button which opens appropriate view.
    
    TODO: Show local file access in FileLocator

--- Main_AndroidSwiftShare/app/src/main/java/com/example/main_androidswiftshare/MainActivity.java
+++ Main_AndroidSwiftShare/app/src/main/java/com/example/main_androidswiftshare/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.main_androidswiftshare;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSend, btnReceive, btnFileLocator;
 
     /** TODO: ITERATION 01 - INITIAL PLACEHOLDER FEATURES (NEAR-FUNCTIONAL) - DEADLINE: MARCH 14, 2025
      * 1.)  REQUEST user to allow LOCAL file access. Written already as manifest uses-permissions.
@@ -33,10 +35,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnSend = findViewById(R.id.sendBTN);
+        btnReceive = findViewById(R.id.receiveBTN);
+        btnFileLocator = findViewById(R.id.filelocatorBTN);
+
+
+        btnSend.setOnClickListener(v -> {
+            // TODO: Add logic for sendFileBTN
+        });
+
+        btnReceive.setOnClickListener(v -> {
+            // TODO: Add logic for receiveFileBTN
+        });
+
+        btnFileLocator.setOnClickListener(v -> {
+
+            // TODO: Add logic for openFileBTN
+
+            Intent intent = new Intent(MainActivity.this, FileLocator.class);
+            startActivity(intent);
+            // Optionally, hide or finish the current MainActivity
+            MainActivity.this.finish();
+
         });
     }
 }
